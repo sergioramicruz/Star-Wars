9886e7ff956b67ac7f0a6828ca87b531004649c0
Sergio <33873097+sergioramicruz@users.noreply.github.com>
2019-02-12 19:03:42 +0100

    Delete servivo.py

--- modulo_final/models/servivo.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields, api
-
-
-class Servivo(models.Model):
-    _name = 'modulo_final.servivo'
-    especie_id = fields.Many2one('modulo_final.especie', string="Especie")
